8b02240cbc639088afedd99da3ca66ae8cea00b0
Maanas Kompalli <maanask@umich.edu>
2023-04-03 19:15:51 +0200

    start of riddles

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class GUI {
-}
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+        AnimationTimer unAutreJour = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+            }
+        };
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            primaryStage.setTitle("Hello world");
+            Group root = new Group();
+            Pane pane = new Pane(root);
+            Scene theScene = new Scene(pane, 600, 400,true);
+            primaryStage.setScene(theScene);
+
+            String[] test={"42","fejiw","@@","dwqew"};
+
+            root.getChildren().add(new Riddle("What's the answer ?",test,0));
+            primaryStage.show();
+            unAutreJour.start();
+
+        }
+
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
--- src/Riddle.java
+++ src/Riddle.java
@@ -1,3 +1,23 @@
-public class Riddle {
-    
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+
+
+public class Riddle extends Pane {
+    private String question;
+    private String[] answer = new String[4];
+    private int rightAnswer;
+
+    Label questionLabel = new Label();
+    public Riddle() {
+        super();
+    }
+
+    public Riddle(String question, String[] answer, int rightAnswer) {
+        this.question = question;
+        this.answer = answer;
+        this.rightAnswer = rightAnswer;
+        questionLabel.setText(question);
+        this.getChildren().add(questionLabel);
+    }
 }
